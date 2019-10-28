5fcd1a7cc756a412aea192274385dfd6126132bb
Maki Hirose <makidigital.sync@gmail.com>
2019-10-28 14:57:55 -0400

    React Router Dom added and page1.js connected to App.js using Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import page1 from "./pages/page1";
+
+// Run "npm start" to start React app.
+// Run "npm i" or "npm i [specific component name]" in the command line if there are any dependencies missing in the node modules folder.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={page1} />
+          {/* <Route exact path="/MainMenu" component={MainMenu} />
+          <Route exact path="/CalorieEntry" component={CalorieEntry} />
+          <Route exact path="/CalorieEntryEdit" component={CalorieEntryEdit} /> */}
+          {/* <Route exact path="/anotherpage/:id" component={AnotherPage} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+//  render={() => (<CalorieEntryEdit calorieData={"Hello There"}/>)}
 
 export default App;
---
+++ src/pages/page1.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './page1.css';
+
+class Page1 extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                Hello, this is Maki's test for page1.js!
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+            </div>
+        );
+    }
+
+}
+
+export default Page1;
