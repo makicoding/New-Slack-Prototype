5695b52a1da1a63a65a256d91c4e9427fa90ee12
Maki Hirose <makidigital.sync@gmail.com>
2019-10-30 19:08:55 -0400

    Screen image successfully added

--- src/components/screen_images/index.js
+++ src/components/screen_images/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import './screenImage.css';
 
-const screenImage = () => {
+const ScreenImage = () => {
     return (
         <div>
-            <img className="screenImage" src={".assets/images/screens_exported_from_sketch_2x_png/HiFi_iPhone_XS_1.png"} alt={"Screen Image"} />
+            <img className="screenImage" src={"./assets/images/screens_exported_from_sketch_2x_png/HiFi_iPhone_XS_1.png"} alt={"Sketch Screen"} />
         </div>
     );
 };
 
-export default screenImage;
+export default ScreenImage;
--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import logo from '../logo.svg';
-import screenImage from '../components/screen_images';
+import ScreenImage from '../components/screen_images';
 import './page1.css';
 
 class Page1 extends React.Component {
 
     render() {
         return (
-            <div className="App">
-            <screenImage />
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                Hello, this is Maki's test for page1.js and page1.css!
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-            </header>
+
+            <div>
+                <ScreenImage />
+
+                <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <p>
+                    Hello, this is Maki's test for page1.js and page1.css!
+                    </p>
+                    <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Learn React
+                    </a>
+                </header>
+                </div>
+
             </div>
         );
     }
