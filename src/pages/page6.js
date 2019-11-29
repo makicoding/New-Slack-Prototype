5811b958bcea8b8b00b61af53e90799cada7717b
Maki Hirose <makidigital.sync@gmail.com>
2019-11-29 08:19:36 -0500

    buttons and js class updated for page6.js

--- src/pages/page6.js
+++ src/pages/page6.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './page5.css';
+import './page6.css';
 import '../components/screen_image/screenImage.css';
 import '../components/page_background/pageBackground.css';
 
 // Run "npm start" to start React app.
 // Run "npm i" or "npm i [specific component name]" in the command line if there are any dependencies missing in the node modules folder.
 
-class Page5 extends React.Component {
+class Page6 extends React.Component {
 
-    p5Button1Click = () => {
+    p6Button1Click = () => {
         window.open(
-            "/page4", "_self"
+            "/page5", "_self"
         );
     }
 
-    p5Button2Click = () => {
+    p6Button2Click = () => {
         window.open(
-            "/page6", "_self"
+            "/page7", "_self"
         );
     }
 
@@ -29,9 +29,9 @@ class Page5 extends React.Component {
                 <div className="screenImageContainer">
                     <img className="screenImage" src={"./assets/images/screens_exported_from_sketch_2x_png/HiFi_iPhone_XS_5.png"} alt={"Sketch Screen"} />
 
-                    <Link className="p5Button1" onClick={this.p5Button1Click}></Link>
+                    <Link className="p6Button1" onClick={this.p6Button1Click}></Link>
 
-                    <Link className="p5Button2" onClick={this.p5Button2Click}></Link>
+                    <Link className="p6Button2" onClick={this.p6Button2Click}></Link>
                 </div>
 
             </div>
@@ -40,4 +40,4 @@ class Page5 extends React.Component {
 
 }
 
-export default Page5;
+export default Page6;
