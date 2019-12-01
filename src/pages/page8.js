fc958fb314e12ffe14739d43091aaa95afe4f2a5
Maki Hirose <makidigital.sync@gmail.com>
2019-12-01 17:33:40 -0500

    page8.js updated

--- src/pages/page8.js
+++ src/pages/page8.js
@@ -7,17 +7,17 @@ import '../components/page_background/pageBackground.css';
 // Run "npm start" to start React app.
 // Run "npm i" or "npm i [specific component name]" in the command line if there are any dependencies missing in the node modules folder.
 
-class Page5 extends React.Component {
+class Page8 extends React.Component {
 
-    p5Button1Click = () => {
+    p8Button1Click = () => {
         window.open(
-            "/page4", "_self"
+            "/page7", "_self"
         );
     }
 
-    p5Button2Click = () => {
+    p8Button2Click = () => {
         window.open(
-            "/page6", "_self"
+            "/page9", "_self"
         );
     }
 
@@ -29,9 +29,9 @@ class Page5 extends React.Component {
                 <div className="screenImageContainer">
                     <img className="screenImage" src={"./assets/images/screens_exported_from_sketch_2x_png/HiFi_iPhone_XS_5.png"} alt={"Sketch Screen"} />
 
-                    <Link className="p5Button1" onClick={this.p5Button1Click}></Link>
+                    <Link className="p5Button1" onClick={this.p8Button1Click}></Link>
 
-                    <Link className="p5Button2" onClick={this.p5Button2Click}></Link>
+                    <Link className="p5Button2" onClick={this.p8Button2Click}></Link>
                 </div>
 
             </div>
@@ -40,4 +40,4 @@ class Page5 extends React.Component {
 
 }
 
-export default Page5;
+export default Page8;
