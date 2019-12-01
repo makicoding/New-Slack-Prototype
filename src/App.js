5ed9d1cd9e29f9b3840d3789b0cc0285443152b0
Maki Hirose <makidigital.sync@gmail.com>
2019-12-01 17:32:21 -0500

    page7 added to react router on app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import page3 from "./pages/page3";
 import page4 from "./pages/page4";
 import page5 from "./pages/page5";
 import page6 from "./pages/page6";
+import page7 from "./pages/page7";
 
 // Run "npm start" to start React app.
 // Run "npm i" or "npm i [specific component name]" in the command line if there are any dependencies missing in the node modules folder.
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/page4" component={page4} />
           <Route exact path="/page5" component={page5} />
           <Route exact path="/page6" component={page6} />
+          <Route exact path="/page7" component={page7} />
           {/* <Route exact path="/MainMenu" component={MainMenu} />
           <Route exact path="/CalorieEntry" component={CalorieEntry} />
           <Route exact path="/CalorieEntryEdit" component={CalorieEntryEdit} /> */}
